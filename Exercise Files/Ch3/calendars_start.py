974bcd298f4de3082c907e719c5f45fa118c1076
mintchococookie <parksungju90@gmail.com>
2020-07-31 16:32:28 -0500

    1

--- Exercise Files/Ch3/calendars_start.py
+++ Exercise Files/Ch3/calendars_start.py
@@ -3,24 +3,50 @@
 #
 
 # import the calendar module
-
+import calendar
 
 # create a plain text calendar
-
+c = calendar.TextCalendar(calendar.SUNDAY) #MONDAY also possible
+st = c.formatmonth(2017, 1, 0, 0)
+print(st)
 
 # create an HTML formatted calendar
-
+hc = calendar.HTMLCalendar(calendar.SUNDAY)
+st = hc.formatmonth(2017, 1)
+print(st)
 
 # loop over the days of a month
 # zeroes mean that the day of the week is in an overlapping month
 
+
+# for i in c.itermonthdays(2017,8):
+#     print(i)
   
+
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
 
+# for month in calendar.month_name:
+#     print(month)
+
+# for day in calendar.day_name:
+#     print(day)
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
   
+print("Team meetings will be on:")
+for m in range(1, 13): # 13 is not inclusive
+    cal = calendar.monthcalendar(2018, m)
+    # print(cal)
+    weekone = cal[0]
+    weektwo = cal[1] #First friday should be within first 2 weeks
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+
+    print("%10s %2d" % (calendar.month_name[m], meetday)) # %10s s:string 10: how many d: integer
--- Exercise Files/Ch3/dates_start.py
+++ Exercise Files/Ch3/dates_start.py
@@ -1,25 +1,33 @@
 #
 # Example file for working with date information
 #
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
+  today = date.today()
+  print("Today's date is ", today)
 
   # print out the date's individual components
+  print("Date Components: ", today.day, today.month, today.year)
+  # Individual day, month year
 
-  
   # retrieve today's weekday (0=Monday, 6=Sunday)
-
+  print("Today's weekday # is : ", today.weekday())
+  days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+  print("which is a: ", days[today.weekday()])
   
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
+  today = datetime.now()
+  print("Today is ", today)
 
   # Get the current time
-
+  print("Time now is: ", datetime.time(datetime.now()))
 
   
 if __name__ == "__main__":
